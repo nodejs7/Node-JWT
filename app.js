6fa6d682036dfaf5463b199ee9246055663d1a44
americanman <snowboardracer7@gmail.com>
2019-06-09 17:50:30 -0400

    Add JWT token route

--- app.js
+++ app.js
@@ -15,4 +15,21 @@ app.post('/api/posts/', (req, res) => {
     message: 'Post created...' 
   });
 })
+
+app.post('/api/login', (req, res) => {
+  // Mock user
+  const user = {
+    id: 1, 
+    username: 'brad',
+    email: 'brad@gmail.com'
+  }
+  // same as jwt.sign({user: user}, 'secretkey',  (err, token) => {
+  jwt.sign({user}, 'secretkey',  (err, token) => {
+    res.json({
+      //same as token: token
+      token
+    })
+  });
+});
+
 app.listen(port, () => console.log('Server started on port 5000'));
