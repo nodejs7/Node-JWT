d2f905cd3b365fe62e5f62a2e34c5fc71edd8abf
americanman <snowboardracer7@gmail.com>
2019-06-09 21:19:09 -0400

    Forbidden if we don't have authorization property in req.header object

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.get('/api', (req, res) => {
   });
 });
 
-app.post('/api/posts/', (req, res) => {
+app.post('/api/posts/', verifyToken, (req, res) => {
   res.json({
     message: 'Post created...' 
   });
@@ -31,5 +31,22 @@ app.post('/api/login', (req, res) => {
     })
   });
 });
+
+// FORMAT OF TOKEN
+// Authorization: Bearer <access_token>
+
+// Verify Token
+function verifyToken(req, res, next) {
+  // Get auth header value
+  const bearerHeader = req.headers['authorization'];
+  // Check if bearer is undefined
+  if(typeof bearerHeader !== 'undefined') {
+
+  } else {
+    // Forbidden
+    res.sendStatus(403);
+  }
+
+}
 
 app.listen(port, () => console.log('Server started on port 5000'));
