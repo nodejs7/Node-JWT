80950a651a0a935ea32c07b8b75b226aa8556a86
americanman <snowboardracer7@gmail.com>
2019-06-10 00:04:33 -0400

    Add happy path to /posts route

--- app.js
+++ app.js
@@ -1,20 +1,27 @@
 const express = require('express');
-const jwt  = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 const port = 5000;
 
 const app = express();
 
 app.get('/api', (req, res) => {
   res.json({
-    message: 'Welcome to the API' 
+    message: 'Welcome to the API'
   });
 });
 
-app.post('/api/posts/', verifyToken, (req, res) => {
-  res.json({
-    message: 'Post created...' 
+app.post('/api/posts', verifyToken, (req, res) => {  
+  jwt.verify(req.token, 'secretkey', (err, authData) => {
+    if(err) {
+      res.sendStatus(403);
+    } else {
+      res.json({
+        message: 'Post created...',
+        authData
+      });
+    }
   });
-})
+});
 
 app.post('/api/login', (req, res) => {
   // Mock user
@@ -23,12 +30,11 @@ app.post('/api/login', (req, res) => {
     username: 'brad',
     email: 'brad@gmail.com'
   }
-  // same as jwt.sign({user: user}, 'secretkey',  (err, token) => {
-  jwt.sign({user}, 'secretkey',  (err, token) => {
+
+  jwt.sign({user}, 'secretkey', (err, token) => {
     res.json({
-      //same as token: token
       token
-    })
+    });
   });
 });
 
@@ -41,7 +47,14 @@ function verifyToken(req, res, next) {
   const bearerHeader = req.headers['authorization'];
   // Check if bearer is undefined
   if(typeof bearerHeader !== 'undefined') {
-
+    // Split at the space
+    const bearer = bearerHeader.split(' ');
+    // Get token from array
+    const bearerToken = bearer[1];
+    // Set the token
+    req.token = bearerToken;
+    // Next middleware
+    next();
   } else {
     // Forbidden
     res.sendStatus(403);
